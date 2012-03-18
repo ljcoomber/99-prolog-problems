ad8fa66665e09780a4d4169e0bf3ebe5e3f7ee77
Lee Coomber <lee@lshift.net>
2012-03-18 19:07:13 +0000

    Solution for P1.02

--- src/p1.pl
+++ src/p1.pl
@@ -1,3 +1,7 @@
 % Find the last element of a list
 my_last(E, [E]) :- !.
 my_last(E, [_|T]) :- my_last(E, T).
+
+% Find the last but one element of a list
+last_but_one(E, [E, _]) :- !.
+last_but_one(E, [_|T]) :- last_but_one(E, T).
--- test/p1.plt
+++ test/p1.plt
@@ -3,4 +3,7 @@
 test(p1_01) :-
     my_last(d, [a, b, c, d]).
 
+test(p1_02) :-
+    last_but_one(c, [a, b, c, d]).
+
 :- end_tests(p1).
