f1162f1fd32847d9b6326ac02029a2e27d92179f
Lee Coomber <lee@lshift.net>
2012-03-18 19:14:35 +0000

    Solution for P1.03

--- src/p1.pl
+++ src/p1.pl
@@ -5,4 +5,10 @@ my_last(X, [_|L]) :- my_last(X, L).
 % Find the last but one element of a list
 last_but_one(X, [X, _]) :- !.
 last_but_one(X, [_,Y|Ys]) :- last_but_one(X, [Y|Ys]).
-    
+
+% Find the K'th element of a list
+element_at(X, [X|_], 1) :- !.
+element_at(X, [H|L], N) :-
+    NewN is N - 1,
+    element_at(X, L, NewN).
+
--- test/p1.plt
+++ test/p1.plt
@@ -6,4 +6,7 @@ test(p1_01) :-
 test(p1_02) :-
     last_but_one(c, [a, b, c, d]).
 
+test(p1_03) :-
+    element_at(c, [a,b,c,d,e], 3).
+
 :- end_tests(p1).
