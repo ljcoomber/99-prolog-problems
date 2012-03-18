6a81d5045365bace23eb9984fb62fc43eed29701
Lee Coomber <lee@lshift.net>
2012-03-18 19:20:53 +0000

    Use real solution var name and add missed condition

--- src/p1.pl
+++ src/p1.pl
@@ -8,7 +8,7 @@ last_but_one(X, [_,Y|Ys]) :- last_but_one(X, [Y|Ys]).
 
 % Find the K'th element of a list
 element_at(X, [X|_], 1) :- !.
-element_at(X, [H|L], N) :-
-    NewN is N - 1,
-    element_at(X, L, NewN).
-
+element_at(X, [H|L], K) :-
+    K > 1,
+    K1 is K - 1,
+    element_at(X, L, K1).
